ff8144fd229f0b6e58d9bc01d695f19dddaf5fce
Gholamhossein Jowkar <ghjowk@yahoo.co.uk>
2023-07-23 13:29:52 +0200

    Modified: Command Line Control (cli) file and some description has been added.

--- setup.py
+++ setup.py
@@ -1,42 +1,42 @@
 ########################################################################################################################
+#
 # Licensed Materials - Property of Gholamhossein Jowkar
-#
-#
-# Copyright (C) 2023-2030 by Gholamhossein Jowkar
-#
-# This work was supported by the Swiss National Science Foundation (SNF) grants $31003A\_176316$ to Dr. M. A.
-# The funding body did not play any role in the design of the study and collection, analysis, and interpretation of
-# data and in writing the code.
+# Copyright (C) 2022-2025 by Gholamhossein Jowkar
 #
 ########################################################################################################################
 #
-# This file is part of IndelViewer of ARPIP project: https://github.com/acg-team/IndelViewer
-#
-#                              ABOUT THE ARPIP PACKAGE
-#                               =====================
+# This file is part of arpip_indel_viewer of ARPIP project: https://github.com/acg-team/IndelViewer
+
+#                           ABOUT THE ARPIP PACKAGE
+#                           =====================
 # ARPIP: Ancestral Sequence Reconstruction with insertions and deletions under the Poisson Indel Process
 # ARPIP is a joint maximum likelihood approach for phylogenetic ancestral sequence reconstruction, capable of modeling
-# indels both biological and mathematically. You can find ARPIP in: https://github.com/acg-team/bpp-arpip
+# indels both biological and mathematically.
 #
+
+# arpip_indel_viewer is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the
+# implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
 #
-# IndelViewer is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied
-# warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
-#
-# IndelViewer is a free software: you can redistribute it and/or modify it under the terms of the GNU Lesser General
-# Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any
-# later version.
+# arpip_indel_viewer is a free software: you can redistribute it and/or modify it under the terms of the GNU Lesser
+# General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your
+# option) any later version.
 #
 # You should have received a copy of the GNU Lesser General Public
-# License along with IndeViewer. If not, see <http://www.gnu.org/licenses/>.
+# License along with IndelViewer. If not, see <http://www.gnu.org/licenses/>.
 #
 ########################################################################################################################
-# Created by:  Gholamhossein Jowkar <jowk@zhaw.ch>
+
+# Created by: Gholam-Hossein Jowkar <jowk@zhaw.ch>
 # ACGT ZHAW
 # Created date: September 2022
 # Modified by:
 # Modified date:
 
 from setuptools import setup, find_packages
+import codecs
+import os
+
+here = os.path.abspath(os.path.dirname(__file__))
 
 with open('README.md') as f:
     readme = f.read()
@@ -57,8 +57,9 @@ setup(
     author_email='jowk@zhaw.ch',
     url='https://github.com/acg-team/IndelViewer',
     license=license,
-    packages=['indelviewer'],
+    packages=find_packages(),
     classifiers=[
+            "Development Status :: 1 - First Version Development",
             "Intended Audience :: Science/Research",
             "Intended Audience :: Developers",
             "Programming Language :: Python",
@@ -72,5 +73,10 @@ setup(
             "Topic :: Scientific/Engineering: Bioinformatics",
             "Topic :: Software Development :: Libraries :: Python Modules",
         ],
-    install_requires=requirements
+    install_requires=requirements,
+    entry_points={
+        'console_scripts': [
+            'add=arpip_indel_viewer._cli:main',
+        ],
+    },
 )
